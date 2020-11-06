547a93a10726e37aa813fcc7ebe878c0ad84a75e
cristinashuw <cristinashuw@gmail.com>
2020-11-06 16:52:12 +0700

    Data Source

---
+++ app/src/main/java/com/cristinashuw/reviewapp/data/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.cristinashuw.reviewapp.data
+
+import androidx.room.Database
+
+//@Database(
+//    version = AppDatabase.DB_VERSION,
+//    exportSchema = false,
+//    entities = [
+//
+//    ]
+//)
+
+abstract class AppDatabase {
+
+}
